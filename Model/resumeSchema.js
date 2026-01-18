deacae1504b2fae23d1b45bfed5e67dd199ba944
Rahinmon2903 <rahin2903@gmail.com>
2026-01-18 12:28:37 +0530

    resume Schema updated

--- Model/resumeSchema.js
+++ Model/resumeSchema.js
@@ -1,4 +1,24 @@
 import mongoose from "mongoose";
 
 
-const resume
+const resumeSchema = new mongoose.Schema({
+    userId: {
+       type:mongoose.Schema.Types.ObjectId,
+       ref:"User",
+       
+    },
+    orginalText: String,
+      
+    
+    parsedData: {
+       skills:[String],
+       experienceLevel:String,
+       domains:[String]
+     } ,
+       version: Number
+},
+      { timestamps: true }
+);
+
+const Resume = mongoose.model('Resume', resumeSchema);
+export default Resume;
