e82d0bd91e5c99ac92c4cac82dfdc800ae48f9b7
Rahinmon2903 <rahin2903@gmail.com>
2026-01-18 12:33:40 +0530

    jobSchema updated

---
+++ Model/jobSchema.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    text: String,
+    extracted: {
+        requiredSkills: [String],
+        preferredSkills: [String],
+        seniority: String,
+        domain: String
+    }
+
+},
+{ timestamps: true }
+
+)
+
+const Job = mongoose.model('Job', jobSchema);
+export default Job
