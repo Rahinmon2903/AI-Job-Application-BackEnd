0b4687a58b9c4e33cf41a7b5825b54a55a1db13e
Rahinmon2903 <rahin2903@gmail.com>
2026-01-18 12:37:27 +0530

    analysis Schema updated

---
+++ Model/analysisSchema.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+
+const analysisSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    jobId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job"
+    },
+    resumeId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Resume"
+    },
+    matchScore: Number,
+    missingSkills: [String],
+    highImpactGaps: [String],
+    verdict: String,
+    explanation: String
+},
+{ timestamps: true }
+)
+const Analysis = mongoose.model('Analysis', analysisSchema);
+export default Analysis;
