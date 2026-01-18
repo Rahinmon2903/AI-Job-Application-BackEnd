de46e3ac5fa7ca629fa303b446a9bf50b7e36b6a
Rahinmon2903 <rahin2903@gmail.com>
2026-01-18 12:43:03 +0530

    register function updated

---
+++ Controller/authController.js
@@ -0,0 +1,24 @@
+import User from "../Model/userSchema.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+
+//register
+export const register = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+        const user = await User.findOne({email});
+        if (user) {
+            return res.status(400).json({message: "User already exists"});
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({name, email, password: hashedPassword});
+        await newUser.save();
+        res.status(201).json({message: "User created successfully"});
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Error in registration"});
+        
+    }
+}
