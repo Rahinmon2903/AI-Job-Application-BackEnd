c4f6dab8fc81f0218fbc6ebae8e377ccf729ba5a
Rahinmon2903 <rahin2903@gmail.com>
2026-01-22 10:38:08 +0530

    indentify missing skills and creating insights

--- Controller/analysisController.js
+++ Controller/analysisController.js
@@ -1,6 +1,7 @@
 import Job from "../Model/jobSchema.js";
 import Resume from "../Model/resumeSchema.js";
 import Analysis from "../Model/analysisSchema.js";
+import Insight from "../Model/insightSchema.js";
 
 // analyze match
 export const analyzeMatch = async (req, res) => {
@@ -58,7 +59,29 @@ export const analyzeMatch = async (req, res) => {
       verdict,
       explanation: `Matched ${matchedSkills.length} out of ${requiredSkills.length} required skills.`
     });
-    
+
+    for(let skill of missingSkills){
+        const insight=await Insight.findOne({
+            userId: req.user._id,
+            skill,
+            type:"high_gap"
+        })
+
+        if(insight){
+            insight.frequency++;
+            insight.lastSeen=new Date();
+            await insight.save();
+        }else{
+            await Insight.create({
+                userId: req.user._id,
+                skill,
+                type:"high_gap",
+                frequency:1,
+                lastSeen:new Date(),
+                message:`You have a high gap in ${skill}`
+            })
+        }
+    }
 
     res.status(201).json({
       message: "Analysis created successfully",
