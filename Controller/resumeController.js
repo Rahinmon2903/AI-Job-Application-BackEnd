9705f6471588aff6ec8bb0a02dd863bc8c0a89e0
Rahinmon2903 <rahin2903@gmail.com>
2026-01-24 09:42:30 +0530

    resume upload function added

--- Controller/resumeController.js
+++ Controller/resumeController.js
@@ -1,4 +1,5 @@
 import Resume from "../Model/resumeSchema.js";
+import pdfParse from "pdf-parse";
 
 
 export const uploadResume = async (req, res) => {
@@ -23,5 +24,33 @@ export const uploadResume = async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "Error in uploading resume" });
+    }
+}
+
+export const uploadResumePdf = async (req, res) => {
+    try {
+          if (!req.file) {
+      return res.status(400).json({ message: "PDF file required" });
+    }
+
+    const pdfData = await pdfParse(req.file.buffer);
+    const resumeText=pdfData.text;
+     //AI output
+        const parsedData = {
+            skills: ["React", "Node.js", "MongoDB"],
+            experienceLevel: "mid",
+            domains: ["Web", "SaaS"]
+        };
+        const lastResume = await Resume.findOne({ userId: req.user._id }).sort({ version: -1 });
+
+        const version = lastResume ? lastResume.version + 1 : 1;
+        const newResume = new Resume({ userId: req.user._id, originalText: resumeText, parsedData,version });
+        await newResume.save();
+        res.status(201).json({ message: "Resume uploaded successfully", resume: newResume });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error in uploading resume PDF" });
     }
 }
