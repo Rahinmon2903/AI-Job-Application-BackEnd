9d6c94796f9f0d41cdf195142a7e6a03e0b1345b
Rahinmon2903 <rahin2903@gmail.com>
2026-01-24 09:45:09 +0530

    route updated

--- Routes/resumeRoute.js
+++ Routes/resumeRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { uploadResume } from "../Controller/resumeController.js";
+import { uploadResume, uploadResumePdf } from "../Controller/resumeController.js";
 import { protect } from "../Middleware/authMiddleware.js";
+import upload from "../Middleware/uploadMiddleware.js";
 
 
 const router=express.Router();
 
 router.post("/",protect, uploadResume);
+router.post("/pdf",protect, upload.single("resume"), uploadResumePdf);
 
 export default router;
