b1175d54feb1a5b868f353fc72c12552ff0626dc
Rahinmon2903 <rahin2903@gmail.com>
2026-01-25 16:56:04 +0530

    analysis route added

--- Router/analysisRoute.js
+++ Router/analysisRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { protect } = require("../Middleware/authMiddleware");
-const { analyzeMatch,getMyAnalysisHistory } = require("../Controller/analysisController");
+const { analyzeMatch,getMyAnalysisHistory,getAnalysisById } = require("../Controller/analysisController");
 
 const router = express.Router();
 
 router.post("/", protect, analyzeMatch);
 router.get("/history", protect, getMyAnalysisHistory);
+router.get("/:id", protect, getAnalysisById);
 
 
 module.exports = router;
